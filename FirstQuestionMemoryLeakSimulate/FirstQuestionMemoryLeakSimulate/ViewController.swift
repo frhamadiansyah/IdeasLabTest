865982bcd3083fd4feb92ce355245186b3f303a7
Fandrian Rhamadiansyah <fandrian.rhamadiansyah@gmail.com>
2024-08-12 17:45:12 +0700

    init commit

---
+++ FirstQuestionMemoryLeakSimulate/FirstQuestionMemoryLeakSimulate/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  FirstQuestionMemoryLeakSimulate
+//
+//  Created by Fandrian Rhamadiansyah on 12/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    lazy var button: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Navigate", for: .normal)
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(button)
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    @objc func buttonTapped() {
+        if let navigate = self.navigationController {
+            navigate.pushViewController(SecondViewController(), animated: true)
+        } else {
+            print("navigation controller nil")
+        }
+    }
+    
+}
+
+class SecondViewController: UIViewController {
+    
+    let main = MainClass()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        main.add(sub: SubClass(main: main))
+    }
+    
+    deinit {
+        print("\(Self.self) object was deallocated")
+    }
+}
+
+class MainClass {
+    var count = 0
+    var subs: [SubClass] = []
+    func add(sub: SubClass) {
+        subs.append(sub)
+    }
+    
+    func heavyCalculation() {
+        for i in 0..<2_500_000 {
+                    count += 1
+                }
+    }
+    
+    deinit {
+        print("\(Self.self) object was deallocated")
+    }
+}
+
+class SubClass {
+    var main: MainClass?
+    
+    init (main: MainClass) {
+        self.main = main
+        self.main?.add(sub: self)
+    }
+    
+    deinit {
+        print("\(Self.self) object was deallocated")
+    }
+}
